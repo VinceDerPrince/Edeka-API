d6ffe01d6615669a5edef12c599cde313b63164d
VinceDerPrince <vincent.elster@gmail.com>
2022-07-12 22:16:22 +0200

    added comments

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import src.scraper as _scraper
 
 app = FastAPI()
 
+#The route of the api, a kind welcome message
 @app.get("/")
 async def route():
     return {"message":"Willkommen zu dieser Edeka Angebote API"}
 
+#The get function which calls the get_offers function from scraper.py
 @app.get("/get_offers")
 async def get_offers(adress: str):
     return _scraper.get_offers(adress)
